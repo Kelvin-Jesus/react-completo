5ef2067c298b36c315adae8f90046c3e72da5d09
Kelvin-Jesus <kj.theprog@gmail.com>
2022-06-17 22:21:22 -0300

    new-class: 0701-prop-types

---
+++ src/06-mais-react/0701-prop-types/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+    return (
+    <>
+        <Button width={300}>Clique</Button>    
+        <Button width={'300'}>Clique</Button>    
+        <Button>Clique</Button>    
+    </>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './App.css';
 import './05-react-router/0606-desafio-router/App.css';
-import App from './05-react-router/0606-desafio-router/App';
+import App from './06-mais-react/0701-prop-types/App';
 // import App from './App';
 
 ReactDOM.render(
